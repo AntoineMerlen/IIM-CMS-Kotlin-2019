749720c4baf33f35dc700bdb7f58f4791ffe46b6
AntoineMerlen <antoine.merlen@outlook.com>
2019-05-25 12:41:32 +0200

    New dump.sql

--- dump.sql
+++ dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le :  ven. 24 mai 2019 à 11:53
+-- Généré le :  sam. 25 mai 2019 à 10:41
 -- Version du serveur :  5.7.23
 -- Version de PHP :  7.2.10
 
@@ -21,9 +21,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `articles` (
-  `id` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `text` text NOT NULL
+                            `id` int(11) NOT NULL,
+                            `title` varchar(255) NOT NULL,
+                            `text` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -31,7 +31,10 @@ CREATE TABLE `articles` (
 --
 
 INSERT INTO `articles` (`id`, `title`, `text`) VALUES
-(2, 'Article 2', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?');
+(6, 'Article 6', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?  Supprimer'),
+(7, 'Article 7', 'adaedeadaedaedad'),
+(8, 'Article 8', 'eafaefoejafefnzrvnzvnrzv'),
+(9, 'Article 9', 'Pas d\'idée de texte désolé !');
 
 -- --------------------------------------------------------
 
@@ -40,9 +43,9 @@ INSERT INTO `articles` (`id`, `title`, `text`) VALUES
 --
 
 CREATE TABLE `comments` (
-  `id` int(11) NOT NULL,
-  `articleId` int(11) NOT NULL,
-  `text` varchar(255) NOT NULL
+                            `id` int(11) NOT NULL,
+                            `articleId` int(11) NOT NULL,
+                            `text` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -50,30 +53,17 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`id`, `articleId`, `text`) VALUES
-(2, 1, 'Frist'),
-(3, 1, 'Bravo pour ce commentaire constructif'),
-(5, 1, 'blabla'),
-(6, 1, 'FFFFFFFF'),
-(7, 1, 'BRAVO !!!'),
-(9, 2, 'Un commentaire ?'),
-(10, 2, 'Comment test'),
-(11, 2, 'GG'),
-(12, 2, 'Bravo'),
-(13, 2, 'Tattata'),
-(14, 2, 'biduke'),
-(15, 2, 'sd'),
-(16, 2, 'sd'),
-(17, 2, 'truc'),
-(18, 1, 'Trop bien cet article !!!'),
-(19, 1, 'Trop bien cet article !!!'),
-(20, 2, 'gg'),
-(21, 1, 'jyh'),
-(22, 1, 'Quel bel article !!!'),
-(23, 1, 'trop biiiiiienn'),
-(24, 1, ''),
-(25, 1, ''),
-(26, 2, 'Bloublou'),
-(27, 4, 'Blabla');
+(30, 7, 'blabla'),
+(31, 7, 'blibli'),
+(32, 6, 'GG'),
+(33, 6, 'Bravo'),
+(34, 6, 'GG'),
+(35, 6, 'Magnifique article !!!'),
+(36, 8, 'AGEZGEZ'),
+(37, 8, 'ada'),
+(39, 8, 'frfrfrfrf'),
+(40, 9, 'Comment 1'),
+(41, 9, 'Comment 2');
 
 --
 -- Index pour les tables déchargées
@@ -83,13 +73,13 @@ INSERT INTO `comments` (`id`, `articleId`, `text`) VALUES
 -- Index pour la table `articles`
 --
 ALTER TABLE `articles`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `comments`
 --
 ALTER TABLE `comments`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -99,10 +89,10 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT pour la table `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT pour la table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
